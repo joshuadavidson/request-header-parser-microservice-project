fc26d02087f2780b65a32552957bccb1d59b44ed
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2016-08-09 18:29:49 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; //set the port to server port or 3000 for local testing
+
+//serve static assets from the public folder
+app.use('/assets', express.static(__dirname + '/public'));
+
+//landing page route
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {
+    root: __dirname + '/public/pages'
+  }, function(err) {
+    if (err) throw err;
+  });
+
+});
+
+//route for handling parseheader requests
+app.get('/parseheader', function(req, res) {
+  res.send("hello!");
+});
+
+app.listen(port);
