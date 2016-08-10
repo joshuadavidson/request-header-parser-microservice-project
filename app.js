c6855792327380563a4fc39d48ada9ce0332a002
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2016-08-10 16:16:22 -0500

    Completed project.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; //set the port to server port or 3000 for local testing
 
+const useragent = require('useragent'); //get useragent package to parse useragent header
+useragent(true);//update useragent with the current RegEx library
+
 //serve static assets from the public folder
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -17,7 +20,11 @@ app.get('/', function(req, res) {
 
 //route for handling parseheader requests
 app.get('/parseheader', function(req, res) {
-  res.send("hello!");
+    res.json({
+    ip: req.ip,
+    lang: req.acceptsLanguages()[0],
+    os: useragent.parse(req.headers['user-agent']).os.toJSON().family
+  });
 });
 
 app.listen(port);
